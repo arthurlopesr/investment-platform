b39998ac036989ae5f21971d329ce4232a65284c
Arthur Lopes <arthurlopr12@gmail.com>
2023-12-04 21:03:45 -0300

    feat: add intestor entity

---
+++  stock-exchange/internal/market/entity/investor.go
@@ -0,0 +1,48 @@
+package entity
+
+type Investor struct {
+	ID            string
+	Name          string
+	AssetPosition []*InvestorAssetPosition
+}
+
+func NewInvestor(id string) *Investor {
+	return &Investor{
+		ID:            id,
+		AssetPosition: []*InvestorAssetPosition{},
+	}
+}
+
+func (i *Investor) AddAssetPosition(assetPosition *InvestorAssetPosition) {
+	i.AssetPosition = append(i.AssetPosition, assetPosition)
+}
+
+func (i *Investor) UpdateAssetPosition(assetId string, qdtShare int) {
+	assetPosition := i.GetAssetPosition(assetId)
+	if assetPosition == nil {
+		i.AssetPosition = append(i.AssetPosition, NewInvestorAssetPosition(assetId, qdtShare))
+	} else {
+		assetPosition.Shares += qdtShare
+	}
+}
+
+func (i *Investor) GetAssetPosition(assetId string) *InvestorAssetPosition {
+	for _, assetPosition := range i.AssetPosition {
+		if assetPosition.AssetID == assetId {
+			return assetPosition
+		}
+	}
+	return nil
+}
+
+type InvestorAssetPosition struct {
+	AssetID string
+	Shares  int
+}
+
+func NewInvestorAssetPosition(assetId string, shares int) *InvestorAssetPosition {
+	return &InvestorAssetPosition{
+		AssetID: assetId,
+		Shares:  shares,
+	}
+}
