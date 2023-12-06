984c778fef77aad22689a1d9288965662e2ade6d
Arthur Lopes <arthurlopr12@gmail.com>
2023-12-05 22:03:57 -0300

    feat: fineshed kafka config

---  stock-exchange/internal/market/transformer/transformer.go
+++  stock-exchange/internal/market/transformer/transformer.go
@@ -17,3 +17,32 @@ func TransformInput(input dto.TradeInput) *entity.Order {
 
 	return order
 }
+
+func TransformOutput(order *entity.Order) *dto.OrderOutput {
+	output := &dto.OrderOutput{
+		OrderId:    order.ID,
+		InvestorId: order.Investor.ID,
+		AssetId:    order.Asset.ID,
+		OrderType:  order.OrderType,
+		Status:     order.Status,
+		Partial:    order.PendingShares,
+		Shares:     order.Shares,
+	}
+
+	var transactionsOutput []*dto.TransactionOutput
+
+	for _, t := range order.Transactions {
+		transactionOutput := &dto.TransactionOutput{
+			TransactionId: t.ID,
+			BuyerId:       t.BuyingOrder.Investor.ID,
+			SellerId:      t.SellingOrder.Investor.ID,
+			AssetId:       t.SellingOrder.Asset.ID,
+			Price:         t.Price,
+			Shares:        t.SellingOrder.Shares - t.SellingOrder.PendingShares,
+		}
+		transactionsOutput = append(transactionsOutput, transactionOutput)
+	}
+
+	output.TransactionOutput = transactionsOutput
+	return output
+}
